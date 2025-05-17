d7010f4ec901b47c4aba825b3d1d5a101fa7ee37
1Franc <139025915+HareYukii@users.noreply.github.com>
2025-05-17 16:40:09 +0900

    Update config.php
    UserID와 Password를 삭제함.

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 define('DSN', 'mysql:host=localhost;dbname=l2writing;charset=utf8');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+define('DB_USER', 'USERNAME');
+define('DB_PASSWORD', 'PASSWORD');
 ?>
